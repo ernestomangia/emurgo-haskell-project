ed047a86d76927c23f0c2459d7b91966e9b9485e
Ernesto <ernesto_mangia@hotmail.com>
2022-08-04 16:44:37 -0400

    Added data types

--- app/DataTypes.hs
+++ app/DataTypes.hs
@@ -1,1 +1,31 @@
 module DataTypes where
+
+type Board            = [ [Cell] ]
+
+type Position         = (Int, Int)
+
+data Cell             = Cell 
+                        {
+                            position          :: Position,
+                            cellDisplayState  :: CellDisplayState,
+                            cellState         :: CellState
+                        } deriving (Show)
+
+data CellDisplayState  = Covered
+                         | Uncovered deriving (Show)
+
+data CellState         = Mine
+                         | AdjacentMine Int deriving (Show)
+
+data GameState         = On 
+                         | Won
+                         | Lost
+
+data Game              = Game 
+                         { 
+                           playerName :: String,
+                           gameState  :: GameState,
+                           board      :: Board,
+                           maxRow     :: Int,
+                           maxCol     :: Int
+                         }
